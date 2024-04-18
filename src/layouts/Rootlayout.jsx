9f60078e318548682b52dfe944150c148921f48d
254binaryninja <139130351+254binaryninja@users.noreply.github.com>
2024-04-19 00:03:53 +0300

    Refactor menu bar styling and add logo to Rootlayout.jsx

--- src/layouts/Rootlayout.jsx
+++ src/layouts/Rootlayout.jsx
@@ -3,24 +3,41 @@ import '../assets/css/root.css'
 export default function RootLayout(){
     return (
         <>
-        <div class="absolute top-0 left-0 w-16 md:w-32 lg:w-48">
-         <img src="src\assets\logo\pixellogo.png" alt=""/>
-           </div>
-           
-       <nav className="absolute top-0 right-0 block sm:inline">
-        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-        <a href="#" class="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a>
+            <div class="absolute top-0 left-0 w-16 md:w-32 lg:w-48 ">
+                <img src="src\assets\logo\pixellogo.png" alt="" />
+            </div>
 
-        <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Company</a>
+            <nav className="absolute top-0 right-0 block sm:inline">
+                <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+                    <NavLink to="#" className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">
+                        Home
+                    </NavLink>
 
-        <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Services</a>
+                    <NavLink
+                        to="#"
+                        className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+                    >
+                        Company
+                    </NavLink>
 
-        <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact</a>
-     </div>
-    </nav>
-    <main>
-        <Outlet/>
-    </main>
+                    <NavLink
+                        to="#"
+                        className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+                    >
+                        Services
+                    </NavLink>
+
+                    <NavLink
+                        to="#"
+                        className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+                    >
+                        Contact
+                    </NavLink>
+                </div>
+            </nav>
+            <main>
+                <Outlet />
+            </main>
         </>
     );
 }
