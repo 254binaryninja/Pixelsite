cd72ff44821ce759da8e05e0dc14db9b76da4d18
254binaryninja <139130351+254binaryninja@users.noreply.github.com>
2024-04-18 23:45:05 +0300

    Refactor menu bar styling and add logo to Rootlayout.jsx

--- src/layouts/Rootlayout.jsx
+++ src/layouts/Rootlayout.jsx
@@ -3,15 +3,24 @@ import '../assets/css/root.css'
 export default function RootLayout(){
     return (
         <>
-            <ul className="menu-bar">
-                <li>Home</li>
-                <li>Contact</li>
-                <li>About</li>
-                <li>Help</li>
-             </ul>
-            <main>
-                <Outlet />
-            </main>
+        <div class="absolute top-0 left-0 w-16 md:w-32 lg:w-48">
+         <img src="src\assets\logo\pixellogo.png" alt=""/>
+           </div>
+           
+       <nav className="absolute top-0 right-0 block sm:inline">
+        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+        <a href="#" class="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a>
+
+        <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Company</a>
+
+        <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Services</a>
+
+        <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact</a>
+     </div>
+    </nav>
+    <main>
+        <Outlet/>
+    </main>
         </>
     );
 }
