0a833f24108b72dc9f17fc9ba8314e5939fe1f9b
254binaryninja <139130351+254binaryninja@users.noreply.github.com>
2024-04-18 14:21:13 +0300

    Update menu bar styling and background image

--- src/layouts/Rootlayout.jsx
+++ src/layouts/Rootlayout.jsx
@@ -1,13 +1,17 @@
 import { NavLink,Outlet } from "react-router-dom";
 import '../assets/css/root.css'
 export default function RootLayout(){
-   
     return (
-<>
-
-<main>
-    <Outlet/>
- </main>
-</>
-    )
+        <>
+            <ul className="menu-bar">
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+                <li>Help</li>
+             </ul>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
 }
