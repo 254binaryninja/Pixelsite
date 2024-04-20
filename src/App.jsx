c8a72b0e127f5fc35002d0cca7f4e67acbcaa8c4
254binaryninja <139130351+254binaryninja@users.noreply.github.com>
2024-04-20 12:25:14 +0300

    Add new pages and update routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import {
   createRoutesFromElements,
   RouterProvider} from 'react-router-dom'
 //Pages
-  import Home from './pages/Home'
+import Home from './pages/Home'
+import Company from './pages/Company'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
   //layouts
  import RootLayout from './layouts/Rootlayout'
+
  const router =createBrowserRouter(
   createRoutesFromElements(
     <Route path = "/" element={<RootLayout/>}>
       <Route index element = {<Home/>}/>
+      <Route path = "/Company" element = {<Company/>}/>
+      <Route path = "/Services" element = {<Services/>}/>
+      <Route path = "/Contact" element = {<Contact/>}/>
     </Route> 
   )
  )
--- src/layouts/Rootlayout.jsx
+++ src/layouts/Rootlayout.jsx
@@ -9,26 +9,26 @@ export default function RootLayout(){
 
             <nav className="absolute top-0 right-0 block sm:inline">
                 <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
-                    <NavLink to="#" className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">
+                    <NavLink to="/" className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">
                         Home
                     </NavLink>
 
                     <NavLink
-                        to="#"
+                        to="/Company"
                         className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
                     >
                         Company
                     </NavLink>
 
                     <NavLink
-                        to="#"
+                        to="/Services" 
                         className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
                     >
                         Services
                     </NavLink>
 
                     <NavLink
-                        to="#"
+                        to="/Contact"
                         className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
                     >
                         Contact
